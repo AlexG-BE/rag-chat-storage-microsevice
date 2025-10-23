386e340927ad2e1e103b44fbffa928c6459ad12a
AlexG-BE <fallen.deluxe+1@gmail.com>
2025-10-23 23:59:44 +0300

    Fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-###############################################################################
-
-alembic upgrade head
-uvicorn "${UVICORN_APP}" --host "${UVICORN_HOST}" --port "${UVICORN_PORT}" --proxy-headers --reload
